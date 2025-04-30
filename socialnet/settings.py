b27f14642a392d2255c933aecc1717f9618f2558
trivoxantem <codewithjosh18@gmail.com>
2025-04-30 09:08:43 -0600

    Update settings.py

--- socialnet/settings.py
+++ socialnet/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,9 +78,10 @@ WSGI_APPLICATION = 'socialnet.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(
-        "postgresql://postgres:hERdbiqilMUYEfIwnpjczzoyegLnzgTJ@shinkansen.proxy.rlwy.net:10006/railway"
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
